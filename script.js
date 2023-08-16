5cc66838aed212329d5eab0183ccf58fa1db6fb4
izanmaizan <izanmaizan@gmail.com>
2023-08-16 17:36:03 +0700

    penyempurnaan

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-let switchFrm = document.querySelector('#switch-frm')
-let switchF1 = document.querySelector('#switch-f1')
-let switchF2 = document.querySelector('#switch-f2')
-let switchCircle = document.querySelectorAll('.switch-circle')
-let switchBtn = document.querySelectorAll('.switch-btn')
-let regFrm = document.querySelector('#register-form')
-let logFrm = document.querySelector('#login-form')
-let allButtons = document.querySelectorAll('.submit')
+let switchCtn = document.querySelector("#switch");
+let switchC1 = document.querySelector("#switch-c1");
+let switchC2 = document.querySelector("#switch-c2");
+let switchCircle = document.querySelectorAll(".switch-circle");
+let switchBtn = document.querySelectorAll(".switch-btn");
+let aContainer = document.querySelector("#regist-container");
+let bContainer = document.querySelector("#login-container");
+let allButtons = document.querySelectorAll(".submit");
 
 let getButtons = (e) => e.preventDefault()
 
 let changeForm = (e) => {
-    switchFrm.classList.add('is-gx')
-    setTimeout(function () {
-        switchFrm.classList.remove('is-gx')
+
+    switchCtn.classList.add("is-gx");
+    setTimeout(function(){
+        switchCtn.classList.remove("is-gx");
     }, 1500)
 
-    switchFrm.classList.toggle('is-txr')
-    switchCircle[0].classList.toggle('is-txr')
-    switchCircle[1].classList.toggle('is-txr')
+    switchCtn.classList.toggle("is-txr");
+    switchCircle[0].classList.toggle("is-txr");
+    switchCircle[1].classList.toggle("is-txr");
 
-    switchF1.classList.toggle('is-hidden')
-    switchF2.classList.toggle('is-hidden')
-    regFrm.classList.toggle('is-txl')
-    logFrm.classList.toggle('is-txl')
-    logFrm.classList.toggle('is-z200')
+    switchC1.classList.toggle("is-hidden");
+    switchC2.classList.toggle("is-hidden");
+    aContainer.classList.toggle("is-txl");
+    bContainer.classList.toggle("is-txl");
+    bContainer.classList.toggle("is-z200");
 }
 
 let mainF = (e) => {
     for (var i = 0; i < allButtons.length; i++)
-        allButtons[i].addEventListener('click', getButtons)
-    for (var i = 0; i < switchBtn.length; i++) 
-        switchBtn[i].addEventListener('click', changeForm)
-};
+        allButtons[i].addEventListener("click", getButtons );
+    for (var i = 0; i < switchBtn.length; i++)
+        switchBtn[i].addEventListener("click", changeForm)
+}
 
-window.addEventListener('load', mainF)
+window.addEventListener("load", mainF);
